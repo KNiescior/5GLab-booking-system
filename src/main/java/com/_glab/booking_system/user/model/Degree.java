6ce7203810519541964ee811347e42d48a238a02
KNiescior <27205@student.kpu.krosno.pl>
2025-09-05 18:10:28 +0200

    Create user createdTimestamp, lastModifiedTimestamp and lastLogin

---
+++ src/main/java/com/_glab/booking_system/user/model/Degree.java
@@ -0,0 +1,11 @@
+package com._glab.booking_system.user.model;
+
+public enum Degree {
+    INZ,
+    MGR,
+    MGR_INZ,
+    DR,
+    DR_INZ,
+    DR_HAB,
+    PROF
+}
--- src/main/java/com/_glab/booking_system/user/model/User.java
+++ src/main/java/com/_glab/booking_system/user/model/User.java
@@ -3,11 +3,13 @@ package com._glab.booking_system.user.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.OffsetDateTime;
 
+@EnableJpaAuditing
 @Entity
 @Table(name = "account")
 @Getter
@@ -18,16 +20,29 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Column(unique = true)
     private String username;
+
     private String password;
+
     private String email;
 
-    @CreationTimestamp
-    private OffsetDateTime createdAt;
+    private String firstName;
 
-    @UpdateTimestamp
-    private OffsetDateTime updatedAt;
+    private String lastName;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Degree degree;
 
     @Temporal(TemporalType.TIMESTAMP)
     private OffsetDateTime lastLogin;
+
+    @CreatedDate
+    @Column(name = "createdTimestamp", updatable = false)
+    private OffsetDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "lastModifiedTimestamp")
+    private OffsetDateTime lastModifiedDate;
+
 }
