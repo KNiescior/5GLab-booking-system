2b6c5d6591ea004e99753444616932fc46cfdd13
KNiescior <27205@student.kpu.krosno.pl>
2025-09-09 16:26:23 +0200

    Force fail test

--- src/test/java/com/_glab/booking_system/BookingSystemApplicationTests.java
+++ src/test/java/com/_glab/booking_system/BookingSystemApplicationTests.java
@@ -3,11 +3,14 @@ package com._glab.booking_system;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class BookingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        fail();
+    }
 
 }
