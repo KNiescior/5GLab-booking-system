9d371a235553f31fd235ba4cd90cacb20d70625a
KNiescior <27205@student.kpu.krosno.pl>
2025-11-26 11:04:17 +0100

    Configure Docker

--- src/test/java/com/_glab/booking_system/user/repository/UserRepositoryTests.java
+++ src/test/java/com/_glab/booking_system/user/repository/UserRepositoryTests.java
@@ -7,11 +7,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@Testcontainers(disabledWithoutDocker = true)
 class UserRepositoryTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+	@DynamicPropertySource
+	static void registerProps(DynamicPropertyRegistry r) {
+		if (postgres.isRunning()) {
+			r.add("spring.datasource.url", postgres::getJdbcUrl);
+			r.add("spring.datasource.username", postgres::getUsername);
+			r.add("spring.datasource.password", postgres::getPassword);
+			r.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+		}
+	}
 
 	@Autowired
 	private TestEntityManager em;
